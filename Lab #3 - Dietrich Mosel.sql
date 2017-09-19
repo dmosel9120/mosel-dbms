c57da24c33ad6cf0b83ded7d5f091a30e3b3136f
dmosel9120 <31635526+dmosel9120@users.noreply.github.com>
2017-09-18 21:27:52 -0400

    Add files via upload

---
+++ Lab #3 - Dietrich Mosel.sql
@@ -0,0 +1,51 @@
+﻿-- Dietrich Mosel -- 
+-- Lab #3 --
+-- Professor Labouseur -- 
+-- September 18th 2017 --
+
+-- #1 -- 
+select ordno, totalusd
+from orders;
+
+-- #2 --
+select name, city
+from agents
+where name = 'Smith';
+
+-- #3 -- 
+select pid, name, priceusd
+from products
+where qty > 200010;
+
+-- #4 --
+select name, city
+from customers
+where city = 'Duluth';
+
+-- #5 --
+select name 
+from agents
+where city not in ('New York', 'Duluth');
+
+-- #6 --
+select *
+from products
+where city not in ('Dallas','Duluth')
+And priceusd >= 1.00;
+
+-- #7 -- 
+select *
+from orders
+where month = 'Mar'
+or month = 'May';
+
+-- #8 -- 
+select *
+from orders 
+where month = 'Feb' 
+And totalusd >= 500.00;
+
+-- #9 -- 
+select *
+from orders
+where cid = 'c005';
